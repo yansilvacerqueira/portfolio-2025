80c1d9b864fecb7999121d9737e32e6c5e0fdea7
Yan Cerqueira <yansilvacerqueira@outlook.com>
2024-12-23 23:19:49 -0300

    feat: finishing the first struct of hero with animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,37 +5,49 @@ import ArrowDown from '@/assets/icons/arrow-down.svg'
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative overflow-x-clip">
+
+      <div className="size-[620px] hero-ring animate-pulse-ring-1" />
+      <div className="size-[820px] hero-ring animate-pulse-ring-2" />
+      <div className="size-[1020px] hero-ring animate-pulse-ring-3" />
+      <div className="size-[1220px] hero-ring animate-pulse-ring-4" />
+
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[100px]" alt='computer-image' />
 
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-3 ">
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-3 mt-8">
             <div className="bg-green-500 size-2.5 rounded-full" />
             <div className="text-sm font-medium">Available for new projects</div>
           </div>
         </div>
 
-        <h1 className="font-serif text-3xl text-center mt-8">Building Execeptional User Experiences</h1>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Execeptional User Experiences
+          </h1>
 
-        <p>
-          I specialize in transforming designs into functional,
-          high-perfoming web applications.
-          Let's discuss your next project.
-        </p>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-perfoming web applications.
+            Let's discuss your next project.
+          </p>
 
-        <div>
-          <button>
-            <span>Explore my work</span>
-            <ArrowDown />
-          </button>
-
-          <button>
-            <span>👋</span>
-            <span>Let's Connect</span>
-          </button>
         </div>
 
+        <div className='flex flex-col md:flex-row justify-center  items-center mt-8 gap-4'>
+          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
+            <span className="font-semibold" >Explore my work</span>
+
+            <ArrowDown className="size-4" />
+          </button>
+
+          <button className='inline-flex items-center gap-2 border border-white bg-white  text-gray-900  px-6 h-12 rounded-xl'>
+            <span>👋</span>
+
+            <span className="font-semibold">Let's Connect</span>
+          </button>
+        </div>
       </div>
     </div>
   );
