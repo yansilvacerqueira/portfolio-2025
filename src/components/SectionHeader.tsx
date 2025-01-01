c5ffa6ff0a46e954bb78fb1f1b38576a9328c4cc
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-01-01 18:13:23 -0300

    fix: fixing  SectionHeader component to use correct colors

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -8,14 +8,14 @@ export const SectionHeader = ({ firstText, title, description }: SectionHeaderPr
   return (
     <>
       <div className="flex justify-center">
-        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-emerald-500">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-gray-800 to-gray-500 text-transparent bg-clip-text">
           {firstText}
         </p>
       </div>
       <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
         {title}
       </h2>
-      <p className="text-center md:text-lg lg:text-xl text-gray-400 mt-4 max-w-md mx-auto">
+      <p className="text-center md:text-lg lg:text-xl text-gray-500 mt-4 max-w-md mx-auto">
         {description}
       </p>
     </>
