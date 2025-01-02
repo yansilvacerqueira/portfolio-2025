d58b2c26002e9507e67de72b5119c0ed016efc51
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-01-02 16:43:22 -0300

    feat: adding contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,32 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-14 lg:py-24 lg:pt-20">
+      <div className="container text-white">
+        <div className="bg-black p-10 rounded-3xl flex flex-col md:flex-row items-center gap-8 md:gap-16">
+
+          <div>
+            <h2 className="sm:text-center md:text-left font-serif text-2xl md:text-3xl">Let's create a contact</h2>
+
+            <p className="sm:text-center md:text-left text-sm md:text-base mt-5">
+              Ready to bring your next project to life ?
+              Let's connect and discuss your next project.
+              How I can help you achieve your goals.
+            </p>
+          </div>
+
+          <div>
+            <button className="bg-slate-100 text-black p-3 inline-flex items-center rounded-lg w-max gap-2">
+              <span className="font-semibold">
+                Contact Me
+              </span>
+
+              <ArrowUpRightIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
