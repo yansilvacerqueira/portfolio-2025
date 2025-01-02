ed9b1a009a7f7b30c4ddb17d0ab131d0a4b8aa0f
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-01-02 17:27:36 -0300

    feat: adding footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,38 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  const year = new Date().getFullYear()
+
+  return (
+    <footer>
+      <div className="container">
+        <div className="py-6 border-t border-black/20 flex flex-col items-center gap-8 md:flex-row md:justify-between">
+          <div>
+            <p className="text-sm">
+              &copy; {year}. All Rights reserverd.
+            </p>
+          </div>
+
+          <nav className="flex flex-col gap-6 md:flex-row">
+            <a href="#" className=" text-black inline-flex items-center gap-2">
+              <span className="font-semibold">
+                Linkedin
+              </span>
+
+              <ArrowUpRightIcon />
+            </a>
+
+            <a href="#" className=" text-black inline-flex items-center gap-2">
+              <span className="font-semibold">
+                Github
+              </span>
+
+              <ArrowUpRightIcon />
+            </a>
+          </nav>
+
+        </div>
+      </div>
+    </footer>
+  );
 };
